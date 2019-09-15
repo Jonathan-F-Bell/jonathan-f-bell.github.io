655a5c5495c0ea718f7c8c1c8d63c00277dcbb0d
Jonathan Bell <jonathanbell@Jonathans-MacBook-Pro.local>
2019-09-15 19:17:47 -0400

    started code for theme switch

--- js/main.js
+++ js/main.js
@@ -6,9 +6,17 @@ var links = document.getElementsByTagName("a");
 var resumeLinks = document.querySelectorAll("#resume a");
 var resumeOpen = 0;
 
+var darkButton = document.getElementById("dark-mode-button");
+var medButton = document.getElementById("med-mode-button");
+var lightButton = document.getElementById("light-mode-button");
+
 
 resumeButton.addEventListener("click", toggleResume);
 closeButton.addEventListener("click", toggleResume);
+
+darkButton.addEventListener("click", function() {colorMode(0) });
+medButton.addEventListener("click", function() {colorMode(1) });
+lightButton.addEventListener("click", function() {colorMode(2) });
 
 function toggleResume() {
     if (resumeOpen == 0) {
@@ -31,5 +39,19 @@ function toggleResume() {
             resumeLinks[i].tabIndex = -1;
         }
         resumeOpen = 0;
+    }
+}
+
+/**
+ * Sets the color mode based on the given number
+ * @param {Integer} mode 0 = dark, 1 = medium, 2 = light 
+ */
+function colorMode(mode) {
+    if (mode == 0) {
+        body.className = "";
+    } else if (mode == 1) {
+        body.className = "med_theme";
+    } else if (mode == 2) {
+        body.className = "light_theme";
     }
 }
